9eb9d467102a495ca81b5c40695ed1484cc137a8
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2021-11-08 14:22:43 +0800

    implement add to cart action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,34 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import CartScreen from './screens/cartScreen';
 import HomeScreen from './screens/HomeScreens';
 import ProductScreen from './screens/ProductScreen';
-
+import {Link} from 'react-router-dom';import { useSelector } from 'react-redux';
+;
 
 function App() {
+
+    const cart = useSelector(state => state.cart);
+    const {cartItems} = cart;
+
   return (
     <BrowserRouter>
     <div className="grid-container">
             <header className="row">
                 <div>
-                    <a className="brand" href="index.html">Testing</a>
+                    <Link className="brand" to="/">Testing</Link>
                 </div>
                 <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/signin">Sign in</a>
+                    <Link to="/cart">Cart
+                    {cartItems.length > 0 && (
+                        <span className="badge">{cartItems.length}</span>
+                    )}
+                    </Link>
+                    <Link to="/signin">Sign in</Link>
                 </div>
             </header>
             <main>
-            <Route path="/cart/:id?" component={CartScreen}></Route>
-              <Route path="/product/:id" component={ProductScreen}></Route>
-              <Route path="/" component={HomeScreen} exact></Route>
+                <Route path="/cart/:id?" component={CartScreen}></Route>
+                <Route path="/product/:id" component={ProductScreen}></Route>
+                <Route path="/" component={HomeScreen} exact></Route>
                   
             </main>
             <footer className="row center">
--- frontend/src/screens/cartScreen.js
+++ frontend/src/screens/cartScreen.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
 
 export default function CartScreen(props) {
     const productId = props.match.params.id;
     const qty = props.location.search 
     ? Number(props.location.search.split('=')[1])
     : 1;
+    const dispatch = useDispatch();
 
-    console.log(props.location.search 
-        ? Number(props.location.search.split('=')[1])
-        : 1)
+    useEffect(() =>{
+        if(productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
+
     return (
         <div>
             <h1>Cart Screen</h1>
