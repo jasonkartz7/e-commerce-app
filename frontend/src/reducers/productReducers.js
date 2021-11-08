4e989d90d949f23a987d12141dea1e9e7655b851
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2021-11-08 12:13:16 +0800

    add redux to productscreen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,10 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { 
+    PRODUCT_DETAILS_FAIL, 
+    PRODUCT_DETAILS_REQUEST, 
+    PRODUCT_DETAILS_SUCCESS, 
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
 export const productListReducer = ( 
     state = { loading: true,products: [] }, 
@@ -10,6 +16,22 @@ export const productListReducer = (
         case PRODUCT_LIST_SUCCESS:
             return { loading: false, products: action.payload };
         case PRODUCT_LIST_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state;
+    }
+};
+
+export const productDetailReducer = ( 
+    state = { loading: false, product: {} }, 
+    action
+    ) => {
+    switch(action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
             return { loading: false, error: action.payload }
         default:
             return state;
