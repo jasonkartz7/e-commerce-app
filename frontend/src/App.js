49c8bfb3cff08ecf23b75046a88365647134d273
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2021-11-09 16:43:02 +0800

    implement signin action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,21 @@ import CartScreen from './screens/cartScreen';
 import HomeScreen from './screens/HomeScreens';
 import ProductScreen from './screens/ProductScreen';
 import {Link} from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import SigninScreen from './screens/SigninScreen';
+import { signout } from './actions/userActions';
 ;
 
 function App() {
 
     const cart = useSelector(state => state.cart);
     const {cartItems} = cart;
-
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo } = userSignin;
+    const dispatch = useDispatch();
+    const signoutHandler = () => {
+        dispatch(signout());
+    };
   return (
     <BrowserRouter>
     <div className="grid-container">
@@ -21,12 +27,28 @@ function App() {
                     <Link className="brand" to="/">Testing</Link>
                 </div>
                 <div>
-                    <Link to="/cart">Cart
-                    {cartItems.length > 0 && (
-                        <span className="badge">{cartItems.length}</span>
-                    )}
+                    <Link to="/cart">
+                        Cart
+                        {cartItems.length > 0 && (
+                            <span className="badge">{cartItems.length}</span>
+                        )}
                     </Link>
-                    <Link to="/signin">Sign in</Link>
+                    {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}            
                 </div>
             </header>
             <main>
--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,20 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
-export default function SigninScreen() {
+export default function SigninScreen(props) {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const redirect = props.location.search
+        ? props.location.search.split('=')[1]
+        :'/';
+
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo, loading, error } = userSignin;
+    
+    const dispatch = useDispatch();
     const submitHandler = (e) => {
         e.preventDefault();
-        
+        dispatch(signin(email, password));
     }
+    useEffect(() => {
+        if(userInfo) {
+            props.history.push(redirect);
+        }
+    },[props.history, redirect, userInfo]);
     return (
         <div>
             <form className="form" onSubmit={submitHandler}>
                 <div>
                     <h1> Sign In </h1>
                 </div>
+                {loading && <LoadingBox></LoadingBox>}
+                {error && <MessageBox variant ="danger"> {error} </MessageBox>}
                 <div>
                     <label htmlFor="email">Email Address</label>
                     <input 
@@ -22,7 +42,7 @@ export default function SigninScreen() {
                         id="email" 
                         placeholder="Enter email" 
                         required
-                        onchange={ e => setEmail(e.target.value)}
+                        onChange={ e => setEmail(e.target.value)}
                     ></input>
                 </div>
                 <div>
@@ -32,7 +52,7 @@ export default function SigninScreen() {
                         id="password" 
                         placeholder="Enter password" 
                         required
-                        onchange={ e => setPassword(e.target.value)}
+                        onChange={ e => setPassword(e.target.value)}
                     ></input>
                 </div>
                 <div>
